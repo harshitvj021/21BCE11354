ee2170c7c3fb552de0a5d3451c8d87c56f299d19
Harshit Singh <120363478+harshitvj021@users.noreply.github.com>
2024-08-25 12:03:30 +0530

    Add files via upload

---
+++ bfhlRoutes.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/', (req, res) =>{
+    const data = req.body.data;
+    const fullName = "Harshit_Singh";
+    const dob = "21012003"; 
+    const user_id = `${fullName}_${dob}`;
+    const email = "harshitsingh_2021@vitbhopal.ac.in";
+    const roll_number = "21BCE11354";
+
+    const numbers = [];
+    const alphabets = [];
+    let highestLowercase = '';
+
+    data.forEach(item => {
+        if (!isNaN(item)) {
+            numbers.push(item);
+        } else {
+            alphabets.push(item);
+            if (item === item.toLowerCase() && item > highestLowercase) {
+                highestLowercase = item;
+            }
+        }
+    });
+
+    const response = {
+        "is_success": true,
+        "user_id": user_id,
+        "email": email,
+        "roll_number": roll_number,
+        "numbers": numbers,
+        "alphabets": alphabets,
+        "highest_lowercase_alphabet": highestLowercase ? [highestLowercase] : []
+    };
+    res.json(response);
+});
+
+router.get('/', (req, res) => {
+    res.status(200).json({ "operation_code": 1 });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bfhlRoutes = require('./bfhlRoutes');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+app.use('/bfhl', bfhlRoutes);
+
+const PORT =  3000
+app.listen(PORT, ()=>{
+    console.log("Server is running on 3000");
+});
